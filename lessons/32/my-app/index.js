eed5fefca6e052e64a932eb5d97afb8ec013e432
Folse <folsefeeder@gmail.com>
2021-09-12 17:35:27 +0300

    new npm files

--- lessons/32/my-app/index.js
+++ lessons/32/my-app/index.js
@@ -1,4 +1,6 @@
 // import functions from your package
-const { smartSearch } = require("folse-32");
+const { smartSearch, randomSearch } = require("folse-lesson-32");
 
 console.log(smartSearch(["Smith", "John"], "John"));
+console.log(randomSearch(["Smith", "John"], "John"));
+console.log(randomSearch([1, 2, 3, 4, 5, 6, 7, 8]));
--- lessons/32/public-package/index.js
+++ lessons/32/public-package/index.js
@@ -1,6 +1,11 @@
 // :)
 const smartSearch = (array, value) => array.includes(value);
+const randomSearch = (array) => {
+  var randomItem = array[Math.floor(Math.random() * array.length)];
+  return randomItem;
+};
 
 module.exports = {
-    smartSearch,
-}
+  smartSearch,
+  randomSearch,
+};
