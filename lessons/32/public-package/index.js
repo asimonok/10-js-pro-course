a503bd80cb61f95d95857017eca8db9b21e58759
ElenaZhukouskaya <elenazhukouskaya@gmail.com>
2021-09-12 14:53:30 +0300

    added randomSearch function

--- lessons/32/public-package/index.js
+++ lessons/32/public-package/index.js
@@ -1,6 +1,17 @@
 // :)
 const smartSearch = (array, value) => array.includes(value);
 
+const randomSearch = (array) => {
+  if (array) {
+    const ind = Math.round(Math.random() * (array.length - 1));
+
+    return array[ind];
+  }
+
+  return null;
+};
+
 module.exports = {
-    smartSearch,
-}
+  smartSearch,
+  randomSearch,
+};
