4f5db735087871a66bd1e8601b2dd339ad0ebf52
Folse <folsefeeder@gmail.com>
2021-09-13 19:20:37 +0300

    test

--- lessons/32/my-app/index.js
+++ lessons/32/my-app/index.js
@@ -4,3 +4,6 @@ const { smartSearch, randomSearch } = require("folse-lesson-32");
 console.log(smartSearch(["Smith", "John"], "John"));
 console.log(randomSearch(["Smith", "John"], "John"));
 console.log(randomSearch([1, 2, 3, 4, 5, 6, 7, 8]));
+
+const _user = 1;
+const i = _user;
