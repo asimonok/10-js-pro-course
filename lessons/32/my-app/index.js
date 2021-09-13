82dfe358202c8c5c999059e7b69d4c9a3b3bd784
ola <o.bukhal@gmail.com>
2021-09-13 16:26:25 +0300

    feat: create new package with dependensy of tms-lesson32

--- lessons/32/my-app/index.js
+++ lessons/32/my-app/index.js
@@ -1,4 +1,6 @@
 // import functions from your package
-const { smartSearch } = require('[NAME_OF_YOUR_PACKAGE]')
+const { smartSearch, smartIndex } = require('tms-lesson32')
+
 
 console.log(smartSearch(['Smith', 'John'], 'John'))
+console.log(smartIndex('Smith', 'th'))
--- lessons/32/public-package/index.js
+++ lessons/32/public-package/index.js
@@ -1,8 +1,7 @@
 // :)
 const smartSearch = (array, value) => array.includes(value);
 const smartIndex = (str, char) => str.indexOf(char);
-console.log(smartIndex('teachMeSkills', 'M'));
-console.log(smartSearch([1,2,3,4], 2));
+
 1
 module.exports = {
     smartSearch,
