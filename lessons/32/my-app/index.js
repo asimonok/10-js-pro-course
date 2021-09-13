4055b68639e243475fddeecdc6f13bafc6bd4495
kate-par <katyaparkhamovich@gmail.com>
2021-09-13 03:02:31 +0300

    changes

--- lessons/32/my-app/index.js
+++ lessons/32/my-app/index.js
@@ -3,6 +3,6 @@ const { smartSearch } = require("lesson-32-parkhamovich-katya");
 
 console.log(smartSearch(["Smith", "John"], "John"));
 
-const { randomSearch } = require("lesson-32-parkhamovich-katya");
+const { randomSearch } = require("lesson-32-parkhamovich-katya');
 
 console.log(randomSearch(1, 10));
