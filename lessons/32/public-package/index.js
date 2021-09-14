61db884dee7c711b79b74dd482c70ff64fb29fef
”Ekaterina <katya.tereshenko.1996@mail.ru>
2021-09-14 17:55:26 +0300

    feat: add new function

--- lessons/32/public-package/index.js
+++ lessons/32/public-package/index.js
@@ -1,6 +1,9 @@
 // :)
 const smartSearch = (array, value) => array.includes(value);
 
+const randomSearch = (min, max) => Math.random() * (max - min) + min;
+
 module.exports = {
-    smartSearch,
-}
+  smartSearch,
+  randomSearch,
+};
