4d0fbd9e0ff1a2993ef956ebb7d561a0b7113537
Folse <folsefeeder@gmail.com>
2021-09-15 02:31:30 +0300

    add switch-case operation, fix mistake

--- lessons/33/try/src/index.js
+++ lessons/33/try/src/index.js
@@ -3,9 +3,8 @@ var input2 = document.querySelector(".number2");
 var button = document.querySelector(".button");
 button.addEventListener("click", function () {
     var input1value = parseFloat(input1.value);
-    var input2value = parseFloat(input1.value);
-    var result = calc(input1value, input2value);
-    consolePrint(result);
+    var input2value = parseFloat(input2.value);
+    consolePrint(calc(input1value, input2value));
 });
 var Operation;
 (function (Operation) {
@@ -19,6 +18,31 @@ function consolePrint(res) {
 }
 function calc(a, b) {
     var select = document.getElementById("select").value;
-    var calced = a + select + b;
-    return eval(calced);
+    var calced = 0;
+    switch (select) {
+        case Operation.sum: {
+            var calcSum = function (a, b) { return a + b; };
+            calced = calcSum(a, b);
+            break;
+        }
+        case Operation.subtraction: {
+            var calcSubtraction = function (a, b) { return a - b; };
+            calced = calcSubtraction(a, b);
+            break;
+        }
+        case Operation.division: {
+            var calcDivision = function (a, b) { return a / b; };
+            calced = calcDivision(a, b);
+            break;
+        }
+        case Operation.multiplication: {
+            var calcMultiply = function (a, b) { return a * b; };
+            calced = calcMultiply(a, b);
+            break;
+        }
+        default: {
+            console.log("Error");
+        }
+    }
+    return calced;
 }
--- lessons/33/try/src/index.ts
+++ lessons/33/try/src/index.ts
@@ -3,14 +3,13 @@ const input2 = document.querySelector(".number2");
 const button = document.querySelector(".button");
 
 button.addEventListener("click", () => {
-  //
   const input1value: number = parseFloat((input1 as HTMLInputElement).value);
-  const input2value: number = parseFloat((input1 as HTMLInputElement).value);
+  const input2value: number = parseFloat((input2 as HTMLInputElement).value);
 
   //   calc(parseFloat((input1 as HTMLInputElement).value), parseFloat((input2 as HTMLInputElement).value));
   //   console.log(calc(input1value, input2value));
-  const result: number = calc(input1value, input2value);
-  consolePrint(result);
+  // const result: number = calc(input1value, input2value);
+  consolePrint(calc(input1value, input2value));
 });
 
 enum Operation {
@@ -26,6 +25,33 @@ function consolePrint(res: number): void {
 
 function calc(a: number, b: number): number {
   const select = (document.getElementById("select") as HTMLSelectElement).value;
-  const calced = a + select + b;
-  return eval(calced);
+  // const calced = a + select + b;
+  let calced: number = 0;
+  switch (select) {
+    case Operation.sum: {
+      const calcSum = (a: number, b: number): number => a + b;
+      calced = calcSum(a, b);
+      break;
+    }
+    case Operation.subtraction: {
+      const calcSubtraction = (a: number, b: number): number => a - b;
+      calced = calcSubtraction(a, b);
+      break;
+    }
+    case Operation.division: {
+      const calcDivision = (a: number, b: number): number => a / b;
+      calced = calcDivision(a, b);
+      break;
+    }
+    case Operation.multiplication: {
+      const calcMultiply = (a: number, b: number): number => a * b;
+      calced = calcMultiply(a, b);
+      break;
+    }
+    default: {
+      console.log("Error");
+    }
+  }
+  // return eval(calced);
+  return calced;
 }
