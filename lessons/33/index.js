326c437edccc6b6c387f9d66770d2c9e98c39fe9
”Ekaterina <katya.tereshenko.1996@mail.ru>
2021-09-14 22:16:08 +0300

    create workspace

---
+++ lessons/33/index.js
@@ -0,0 +1,48 @@
+//alert('Hello');
+var inputEl1 = document.querySelector('.value1');
+var inputEl2 = document.querySelector('.value2');
+var buttonEl = document.querySelector('.btn');
+var selectEl = document.querySelector('.operation');
+var Operations;
+(function (Operations) {
+    Operations["SUM"] = "+";
+    Operations["SUB"] = "-";
+    Operations["DIV"] = "/";
+    Operations["MUL"] = "*";
+})(Operations || (Operations = {}));
+var calcSum = function (a, b) { return a + b; };
+var calcSub = function (a, b) { return a - b; };
+var calcDiv = function (a, b) { return a / b; };
+var calcMult = function (a, b) { return a * b; };
+buttonEl.addEventListener('click', function () {
+    var inputVal1 = parseFloat(inputEl1.value);
+    var inputVal2 = parseFloat(inputEl2.value);
+    var selectVal = selectEl.value;
+    var result = 0;
+    if (isNaN(inputVal1) || isNaN(inputVal2)) {
+        console.log('Wrong values');
+        return;
+    }
+    switch (selectVal) {
+        case Operations.SUM: {
+            result = calcSum(inputVal1, inputVal2);
+            break;
+        }
+        case Operations.SUB: {
+            result = calcSub(inputVal1, inputVal2);
+            break;
+        }
+        case Operations.DIV: {
+            result = calcDiv(inputVal1, inputVal2);
+            break;
+        }
+        case Operations.MUL: {
+            result = calcMult(inputVal1, inputVal2);
+            break;
+        }
+        default: {
+            console.log('Unable to conduct operation');
+        }
+    }
+    console.log(result);
+});
---
+++ lessons/33/src/index.ts
@@ -0,0 +1,54 @@
+//alert('Hello');
+
+const inputEl1 = document.querySelector('.value1') as HTMLInputElement;
+const inputEl2 = document.querySelector('.value2') as HTMLInputElement;
+const buttonEl = document.querySelector('.btn') as HTMLButtonElement;
+const selectEl = document.querySelector('.operation') as HTMLSelectElement;
+
+enum Operations {
+  SUM = '+',
+  SUB = '-',
+  DIV = '/',
+  MUL = '*',
+}
+
+const calcSum = (a: number, b: number): number => a + b;
+const calcSub = (a: number, b: number): number => a - b;
+const calcDiv = (a: number, b: number): number => a / b;
+const calcMult = (a: number, b: number): number => a * b;
+
+buttonEl.addEventListener('click', () => {
+  const inputVal1: number = parseFloat(inputEl1.value);
+  const inputVal2: number = parseFloat(inputEl2.value);
+  const selectVal: string = selectEl.value;
+  let result: number = 0;
+
+  if (isNaN(inputVal1) || isNaN(inputVal2)) {
+    console.log('Wrong values');
+    return;
+  }
+
+  switch (selectVal) {
+    case Operations.SUM: {
+      result = calcSum(inputVal1, inputVal2);
+      break;
+    }
+    case Operations.SUB: {
+      result = calcSub(inputVal1, inputVal2);
+      break;
+    }
+    case Operations.DIV: {
+      result = calcDiv(inputVal1, inputVal2);
+      break;
+    }
+    case Operations.MUL: {
+      result = calcMult(inputVal1, inputVal2);
+      break;
+    }
+    default: {
+      console.log('Unable to conduct operation');
+    }
+  }
+
+  console.log(result);
+});
