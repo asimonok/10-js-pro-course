ff2d62e91faa4bdc84c0717db5a9f5d445e9be37
kate-par <katyaparkhamovich@gmail.com>
2021-09-15 01:25:44 +0300

    hw is done

--- lessons/33/my-app-ts/index.js
+++ lessons/33/my-app-ts/index.js
@@ -1,3 +1,44 @@
-var message = "Hello World";
-console.log(message);
+var input1El = document.querySelector(".input-1");
+var input2El = document.querySelector(".input-2");
+var buttonEl = document.querySelector(".submit-button");
+var selectEl = document.querySelector(".operation-select");
+var calcAdd = function (a, b) { return a + b; };
+var calcSub = function (a, b) { return a - b; };
+var calcDiv = function (a, b) { return a / b; };
+var calcMult = function (a, b) { return a * b; };
+var Operation;
+(function (Operation) {
+    Operation["add"] = "add";
+    Operation["sub"] = "sub";
+    Operation["div"] = "div";
+    Operation["mult"] = "mult";
+})(Operation || (Operation = {}));
+buttonEl.addEventListener("click", function () {
+    var input1Value = parseFloat(input1El.value);
+    var input2Value = parseFloat(input2El.value);
+    var selectValue = selectEl.value;
+    var result = 0;
+    switch (selectValue) {
+        case Operation.add: {
+            result = calcAdd(input1Value, input2Value);
+            break;
+        }
+        case Operation.sub: {
+            result = calcSub(input1Value, input2Value);
+            break;
+        }
+        case Operation.div: {
+            result = calcDiv(input1Value, input2Value);
+            break;
+        }
+        case Operation.mult: {
+            result = calcMult(input1Value, input2Value);
+            break;
+        }
+        default: {
+            alert("error");
+        }
+    }
+    alert(result);
+});
 //# sourceMappingURL=index.js.map
--- lessons/33/my-app-ts/src/index.ts
+++ lessons/33/my-app-ts/src/index.ts
@@ -1,2 +1,49 @@
-let message:string = "Hello World" 
-console.log(message);
+const input1El = document.querySelector(".input-1") as HTMLInputElement;
+const input2El = document.querySelector(".input-2") as HTMLInputElement;
+const buttonEl = document.querySelector(".submit-button") as HTMLButtonElement;
+const selectEl = document.querySelector(
+  ".operation-select"
+) as HTMLSelectElement;
+
+const calcAdd = (a: number, b: number): number => a + b;
+const calcSub = (a: number, b: number): number => a - b;
+const calcDiv = (a: number, b: number): number => a / b;
+const calcMult = (a: number, b: number): number => a * b;
+
+enum Operation {
+  add = "add",
+  sub = "sub",
+  div = "div",
+  mult = "mult",
+}
+
+buttonEl.addEventListener("click", () => {
+  const input1Value: number = parseFloat(input1El.value);
+  const input2Value: number = parseFloat(input2El.value);
+  const selectValue: string = selectEl.value;
+  let result: number = 0;
+
+  switch (selectValue) {
+    case Operation.add: {
+      result = calcAdd(input1Value, input2Value);
+      break;
+    }
+    case Operation.sub: {
+      result = calcSub(input1Value, input2Value);
+      break;
+    }
+    case Operation.div: {
+      result = calcDiv(input1Value, input2Value);
+      break;
+    }
+    case Operation.mult: {
+      result = calcMult(input1Value, input2Value);
+      break;
+    }
+    default: {
+      alert("error");
+    }
+  }
+
+  alert(result);
+});
