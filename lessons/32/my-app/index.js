8659c3f065bc30b1c5523aab15cde8235eea365e
volha bukhal <o.bukhal@gmail.com>
2021-09-14 23:46:59 +0300

    fix: config husky

--- lessons/32/my-app/index.js
+++ lessons/32/my-app/index.js
@@ -1,10 +1,10 @@
 // import functions from your package
-const { smartSearch, smartIndex } = require("tms-lesson32");
+const { smartSearch, smartIndex } = require('tms-lesson32');
 
-console.log(smartSearch(["Smith", "John"], "John'));
-console.log(smartIndex("Smith", "th"));
+console.log(smartSearch(['Smith', 'John'], 'John'));
+console.log(smartIndex('Smith', 'th'));
 
 function printArr(arr) {
   console.log(arr);
 }
-   printArr(["Smith", "John"])
+printArr(['Smith', 'John']);
