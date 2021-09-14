8bc5b6738064e727cc199b92a132062cfa0a0cac
volha bukhal <o.bukhal@gmail.com>
2021-09-14 22:45:08 +0300

    add wrong sting for check

--- lessons/32/my-app/index.js
+++ lessons/32/my-app/index.js
@@ -3,3 +3,8 @@ const { smartSearch, smartIndex } = require('tms-lesson32');
 
 console.log(smartSearch(['Smith', 'John'], 'John'));
 console.log(smartIndex('Smith', 'th'));
+
+
+         function printArr(arr) {
+    console.log(arr);
+};  
