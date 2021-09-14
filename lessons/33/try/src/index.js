b6895160b7a8392bd0cca374fd796e4de2cc4d19
Folse <folsefeeder@gmail.com>
2021-09-14 22:16:08 +0300

    add new function and enum

--- lessons/33/try/src/index.js
+++ lessons/33/try/src/index.js
@@ -4,8 +4,19 @@ var button = document.querySelector(".button");
 button.addEventListener("click", function () {
     var input1value = parseFloat(input1.value);
     var input2value = parseFloat(input1.value);
-    console.log(calc(input1value, input2value));
+    var result = calc(input1value, input2value);
+    consolePrint(result);
 });
+var Operation;
+(function (Operation) {
+    Operation["sum"] = "+";
+    Operation["subtraction"] = "-";
+    Operation["division"] = "/";
+    Operation["multiplication"] = "*";
+})(Operation || (Operation = {}));
+function consolePrint(res) {
+    console.log(res);
+}
 function calc(a, b) {
     var select = document.getElementById("select").value;
     var calced = a + select + b;
--- lessons/33/try/src/index.ts
+++ lessons/33/try/src/index.ts
@@ -8,8 +8,21 @@ button.addEventListener("click", () => {
   const input2value: number = parseFloat((input1 as HTMLInputElement).value);
 
   //   calc(parseFloat((input1 as HTMLInputElement).value), parseFloat((input2 as HTMLInputElement).value));
-  console.log(calc(input1value, input2value));
+  //   console.log(calc(input1value, input2value));
+  const result: number = calc(input1value, input2value);
+  consolePrint(result);
 });
+
+enum Operation {
+  sum = "+",
+  subtraction = "-",
+  division = "/",
+  multiplication = "*",
+}
+
+function consolePrint(res: number): void {
+  console.log(res);
+}
 
 function calc(a: number, b: number): number {
   const select = (document.getElementById("select") as HTMLSelectElement).value;
