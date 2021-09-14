7f1eb8331d0cfa4d51a3a175084a42574ed09791
ola <o.bukhal@gmail.com>
2021-09-14 15:32:49 +0300

    fix: update package with beta tag

--- lessons/32/public-package/index.js
+++ lessons/32/public-package/index.js
@@ -2,7 +2,10 @@
 const smartSearch = (array, value) => array.includes(value);
 const smartIndex = (str, char) => str.indexOf(char);
 
-1
+console.log(smartSearch([1,2,3,4], 2));
+console.log(smartIndex('teachMeSkills', 'M'));
+
+
 module.exports = {
     smartSearch,
     smartIndex,
