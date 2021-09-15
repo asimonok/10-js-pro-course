0f9bed92fa2968b6c98fc349eac83307807d1e2b
Iriuna <irenserge@mail.ru>
2021-09-15 10:15:38 +0300

    feat(lesson32) add my npm package

--- lessons/32/my-app/index.js
+++ lessons/32/my-app/index.js
@@ -1,4 +1,8 @@
 // import functions from your package
-const { smartSearch } = require('[NAME_OF_YOUR_PACKAGE]')
+const _smartSearch  = require('lodash/uniq');
 
-console.log(smartSearch(['Smith', 'John'], 'John'))
+const mySearch = (array) => _smartSearch(array);
+
+console.log(_smartSearch(['Smith', 'John'], 'John'));
+
+module.exports = mySearch;
