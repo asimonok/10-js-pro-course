cd093f28838a5dbf3405a396978d30d3baa67bf4
Folse <folsefeeder@gmail.com>
2021-09-17 02:52:43 +0300

    add html code and start ts code

--- lessons/34/calculator/src/index.ts
+++ lessons/34/calculator/src/index.ts
@@ -1,1 +1,38 @@
-console.log("hello");
+const inputSalaryEl = document.querySelector(".input-salary");
+const inputVacationEl = document.querySelector(".input-vacation");
+
+const select = (document.getElementById("currency") as HTMLSelectElement).value;
+// const inputSalaryElValue: number = parseFloat((inputSalaryEl as HTMLInputElement).value);
+// const inputVacationElValue: number = parseFloat((inputVacationEl as HTMLInputElement).value);
+
+const salaryYearEl = document.querySelector(".salary-in-year-value");
+const salaryVacationEl = document.querySelector(".salary-in-vacation-value");
+const salaryWeekEl = document.querySelector(".salary-in-week-value");
+const salaryDayEl = document.querySelector(".salary-in-day-value");
+const salaryHourEl = document.querySelector(".salary-in-hour-value");
+const salaryMinuteEl = document.querySelector(".salary-in-minute-value");
+const salaryRnEl = document.querySelector(".salary-rn-value");
+
+function calcSalaryYear(): number {
+  const inputSalaryElValue: number = parseFloat((inputSalaryEl as HTMLInputElement).value);
+  return inputSalaryElValue * 12;
+}
+
+// setInterval(calcSalaryYear, 1000);
+// console.log(calcSalaryYear());
+
+function printResults(): void {
+  //   let salaryYearElValue: string = salaryYearEl.innerHTML;
+  salaryYearEl.innerHTML = calcSalaryYear() + "";
+}
+
+// console.log(setInterval(calcSalaryYear, 1000));
+// var myVar = setInterval(function () {
+//   printResults();
+// }, 1000);
+
+const buttonEl = document.querySelector(".button");
+buttonEl.addEventListener("click", () => {
+  console.log(calcSalaryYear());
+  printResults();
+});
