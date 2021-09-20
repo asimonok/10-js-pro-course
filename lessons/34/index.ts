aeb9a9854960f51ae4bc25860743c0c53dd1b599
Irina <irenserge@mail.ru>
2021-09-20 15:13:06 +0300

    fix(hw34) finish index.ts file

--- lessons/34/index.ts
+++ lessons/34/index.ts
@@ -1,5 +1,71 @@
-console.log("Настройка завершена");
 
+// initial data for calculation
 let salaryEl = document.querySelector('.salary-sum') as HTMLInputElement;
+let currencyEl = document.querySelector('#currency') as HTMLSelectElement;
+let chosenEl = document.querySelectorAll('.chosen');
+let dayWorkedEl = document.querySelector('#week-worked') as HTMLSelectElement;
+let timeWorkedEl = document.querySelector('#time-worked') as HTMLSelectElement;
+let vocationDaysEl = document.querySelector('.vocation-days') as HTMLInputElement;
 
-console.log(salaryEl.value);
+// calculated data
+let resultSumEl: NodeListOf<Element> = document.querySelectorAll('.result-sum');
+let yearSumyEl = document.querySelector('#year');
+let vocationSumEl = document.querySelector('#vocation');
+let weekSumEl = document.querySelector('#week');
+let daySumEl = document.querySelector('#day');
+let hourSumEl = document.querySelector('#hour');
+let minuteSumyEl = document.querySelector('#minute');
+let nowSumEl = document.querySelector('#now');
+
+
+const currency = {
+    'rub': 1,
+    'euro': 3.1,
+    'dollar': 2.5,
+    'hryvnia': 0.093,
+  };
+
+  // currency change in text
+  currencyEl.addEventListener( 'change', () => {
+    chosenEl.forEach(elem => elem.textContent = currencyEl.options[currencyEl.selectedIndex].text)
+  } );
+
+  //count result block
+const countSum = (period: string): number => {
+    let dayEarned: number = +salaryEl.value / (+dayWorkedEl.value * 4);
+    let hourEarned: number = dayEarned / +timeWorkedEl.value;
+
+    if(period === 'year') {
+        return +salaryEl.value * 12;
+    }
+    if(period === 'vocation'){
+        return  dayEarned * +vocationDaysEl.value;
+    }
+    if(period === 'week') {
+        return Math.round( +salaryEl.value / 4 );
+    }
+    if(period === 'day') {
+        return Math.round( dayEarned );
+    }
+    if(period === 'hour') {
+        return Math.round( hourEarned );
+    }
+    if(period === 'minute') {
+        return +( hourEarned / 60 ).toFixed(2);
+    }
+    if(period === 'now') {
+        return +( hourEarned / (60 * 60) ).toFixed(3);
+    }
+}
+
+ // recording results after DOM loaded
+document.addEventListener('DOMContentLoaded', () => {
+    resultSumEl.forEach(elem => elem.textContent = countSum(elem.id).toString());
+});
+
+// update rusults when started data was changed
+document
+    .querySelectorAll('[data-start]')
+    .forEach(elem => elem.addEventListener('input', () => {
+        resultSumEl.forEach(elem => elem.textContent = countSum(elem.id).toString());
+    } ))
