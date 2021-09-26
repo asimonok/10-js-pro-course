eaaeb4e1bf5d0aa7f61603742298932d16f36157
kate-par <katyaparkhamovich@gmail.com>
2021-09-26 21:00:11 +0300

    create of components

--- lessons/36/src/index.ts
+++ lessons/36/src/index.ts
@@ -55,3 +55,50 @@ class Model {
 }
 
 const model = new Model({ siteName: "", pathname: window.location.pathname });
+
+const createComponent = (stringHtml: string): ChildNode => {
+  const bodyElement = new DOMParser()
+    .parseFromString(stringHtml, "text/html")
+    .querySelector("body") as HTMLBodyElement;
+  return bodyElement.firstChild as ChildNode;
+};
+
+const onNavigateToPage = (event: Event) => {
+  if (event.target) {
+    const dataset = (event.target as HTMLButtonElement).dataset as {
+      href: string;
+    };
+    window.history.pushState(null, dataset.href, dataset.href);
+    model.update({ pathname: window.location.pathname });
+  }
+};
+
+window.onNavigateToPage = onNavigateToPage;
+
+const Header = ({ siteName }: State) =>
+  createComponent(`
+  <header class="header">
+    <span class="logo">${siteName}</span>
+    <a data-href="/index" onClick="onNavigateToPage(event)">Home</a>
+    <a data-href="/contacts">Contacts</a>
+  </header>
+`);
+
+const onChangeName = (event: Event) => {
+  if (event.target) {
+    const value = (event.target as HTMLInputElement).value;
+    model.update({ siteName: value });
+  }
+};
+
+window.onChangeName = onChangeName;
+
+const Main = (params: State) =>
+  createComponent(`
+  <main class="content">
+    <input class="site-name" placeholder="Site name" value="${params.siteName}" onChange="onChangeName(event)" />
+    <h1>My app</h1>
+    <p>Some text</p>
+    <img src="cat.jpeg" />
+  </main>
+`);
