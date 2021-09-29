db8fa5dbddd922faa0fa8605925b03b74ae64a28
volha bukhal <o.bukhal@gmail.com>
2021-09-29 14:40:11 +0300

    feat: add logo change

--- lessons/36/build/index.js
+++ lessons/36/build/index.js
@@ -1,36 +1,57 @@
 "use strict";
-const siteNameElement = document.querySelector('.site-name');
-const logoElement = document.querySelector('.logo');
+class Model {
+    constructor(initialState) {
+        this.state = initialState;
+        this.subscribers = [];
+    }
+    update(updatedState) {
+        this.state = Object.assign(Object.assign({}, this.state), updatedState);
+        this.subscribers.forEach((callback) => {
+            callback(this.state);
+        });
+    }
+    subscribe(callback) {
+        const subscriber = (state) => callback(state);
+        this.subscribers.push(subscriber);
+        return () => {
+            this.subscribers = this.subscribers.filter((item) => item !== subscriber);
+        };
+    }
+}
+const model = new Model({ siteName: 'My app' });
 const createComponent = (stringHtml) => {
     const bodyElement = new DOMParser().parseFromString(stringHtml, 'text/html').querySelector('body');
     return bodyElement.firstChild;
 };
-const model = {
-    siteName: 'my app',
-};
-const Header = ({ siteName: State }) => createComponent(`
-    <header class="header">
+const Header = ({ siteName }) => createComponent(`<header class="header">
     <span class="logo">${siteName}</span>
     <a href="/index">Home</a>
     <a href="/contacts">Contacts</a>
     </header>`);
-const Main = (params) => createComponent;
-(`<main class="content">
-    <input class="site-name" placeholder="Site name" />
+const onChangeName = (event) => {
+    if (event.target) {
+        const value = event.target.value;
+        model.update({ siteName: value });
+        console.log(value);
+        console.log(model.state);
+    }
+};
+window.onChangeName = onChangeName;
+const Main = ({ siteName }) => createComponent(`<main class="content">
+    <input class="site-name" placeholder="Site name" onchange="onChangeName(event)" />
     <h1>My app</h1>
     <p>Some text</p>
     <img src="cat.jpeg" />
     </main>
    `);
-const onChangeName = (event) => {
-    if (event.target) {
-        const value = event.target.value;
-    }
-};
-window.onChangeName = onChangeName;
-const render = (rootElement) => {
+const render = (rootElement, model) => {
     rootElement.innerHTML = '';
     rootElement.appendChild(Header(model));
     rootElement.appendChild(Main(model));
 };
-render(document.querySelector('#app'));
+render(document.querySelector('#app'), model.state);
+model.update({ siteName: '1we' });
+model.update({ siteName: '235' });
+const unsubscribe = model.subscribe((state) => {
+    render(document.querySelector('#app'), state);
+});
--- lessons/36/src/index.ts
+++ lessons/36/src/index.ts
@@ -1,34 +1,68 @@
-const siteNameElement = document.querySelector<HTMLInputElement>('.site-name');
-const logoElement = document.querySelector('.logo');
+class Model {
+    state: State;
+    subscribers: Array<(state: State) => void>;
 
-const createComponent = (stringHtml: string): ChildNode => {
-    const bodyElement = new DOMParser().parseFromString(stringHtml,  'text/html').querySelector('body') as HTMLBodyElement;
-        return bodyElement.firstChild as ChildNode;
-};
+    constructor(initialState: State) {
+        this.state = initialState;
+        this.subscribers = []; 
+    }
+
+    update (updatedState: Partial<State>) {
+        this.state = {
+            ...this.state,
+            ...updatedState,
+        } 
+        
+        this.subscribers.forEach( (callback) => {
+            callback(this.state);
+        })
+    }
+
+    subscribe (callback: (state: State) => void) {
+        const subscriber = (state: State) => callback(state);
+        this.subscribers.push(subscriber);
+        return () => {
+            this.subscribers = this.subscribers.filter((item) => item !== subscriber);
+        }
+    }
+}
+
+const model = new Model({siteName: 'My app'})
 
 interface State {
     siteName: string;
 }
 
-declare interface Window {
-    onChangeName: (event: Event) => void;
+const createComponent = (stringHtml: string): ChildNode => {
+    const bodyElement = new DOMParser().parseFromString(stringHtml, 'text/html').querySelector('body') as HTMLBodyElement;
+        return bodyElement.firstChild as ChildNode;
 }
 
-const model: State = {
-    siteName: 'my app',
-}; 
-
-const Header = ({siteName: State}) => createComponent( 
-    `
-    <header class="header">
+const Header = ({siteName}: State) => createComponent( 
+    `<header class="header">
     <span class="logo">${siteName}</span>
     <a href="/index">Home</a>
     <a href="/contacts">Contacts</a>
     </header>`
 )
-const Main = (params: State ) createComponent( 
+
+// instead of controller make simple function
+const onChangeName = (event: Event) => {
+    if(event.target) {
+        const value = (event.target as HTMLInputElement).value;
+        model.update({siteName: value})
+        console.log(value);
+        console.log(model.state);
+    }
+};
+declare interface Window {
+    onChangeName: (event: Event) => void;
+}
+window.onChangeName = onChangeName;
+
+const Main = ({siteName}: State) => createComponent( 
     `<main class="content">
-    <input class="site-name" placeholder="Site name" />
+    <input class="site-name" placeholder="Site name" onchange="onChangeName(event)" />
     <h1>My app</h1>
     <p>Some text</p>
     <img src="cat.jpeg" />
@@ -36,37 +70,41 @@ const Main = (params: State ) createComponent(
    `
 );
 
-const onChangeName = (event: Event) => {
-    if(event.target) {
-        const value = (event.target as HTMLInputElement).value;
-    }
-};
-
-window.onChangeName = onChangeName;
 
 
-
-
-
-
-
-
-const render = (rootElement: HTMLElement): void => {
+const render = (rootElement: HTMLElement, model: State): void => {
     rootElement.innerHTML = ''; //clear root first
     rootElement.appendChild(Header(model));  
     rootElement.appendChild(Main(model));
-} 
+}
 
 render(
-    document.querySelector('#app') as HTMLElement,
+    document.querySelector('#app') as HTMLElement, 
+    model.state
 );
 
 
+model.update({siteName: '1we'});
+model.update({siteName: '235'});
 
-// if (siteNameElement) {
-//     siteNameElement.addEventListener('input', (event) => {
-//         if (event.target && logoElement) {
-//             logoElement.textContent = (event.target as HTMLInputElement).value;
-//         }
-//     })
-// }
+
+const unsubscribe = model.subscribe( (state: State) => {
+    render(
+        document.querySelector('#app') as HTMLElement, 
+        state
+    );
+});
+
+
+
+
+// model.update({siteName: '1we'});
+// model.update({siteName: '235'});
+// // unsubscribe();
+// model.update({siteName: '1we'});
+
+
+// console.log('subscribers: ',model.subscribers);
+
+// console.log('subscribers: ',model.subscribers);
+
