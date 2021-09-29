a432ab7cc539502d11b044580db7289e07bff79e
Folse <folsefeeder@gmail.com>
2021-09-29 03:05:16 +0300

    add some feat

--- lessons/37/second-try/my-app/src/App.tsx
+++ lessons/37/second-try/my-app/src/App.tsx
@@ -7,32 +7,38 @@ import { CardList } from "./components/Card-list/Card-list";
 import { Price } from "./components/Price/price";
 
 const App: React.FC = () => {
-  const lists = [1, 2, 3];
-  const lists2 = [1, 2, 3, 4, 5];
+  const lists = ["8 hours usage of our coworking space", "Access to All our rooms"];
+  const lists2 = [
+    "8 hours usage of our coworking space",
+    "Access to All our rooms",
+    "Dedicated Desk",
+    "Free Business Address",
+    "Free Lunch 1x a day",
+  ];
   return (
     <div className="App">
       <Header />
 
       <div className="cards">
         <div className="card-1 green darken-2">
-          <CardTitle text="Save More With Goodplans" />
+          <CardTitle text=" Save More With Goodplans" icon={false} />
           <CardH2 text="With Goodplans" />
           <i className="material-icons">arrow_forward</i>
         </div>
         <div className="card-2">
-          <CardTitle text="Day Pass" />
+          <CardTitle text=" Day Pass" icon={true} />
           <CardH2 text="What You'll Get" />
           <CardList lists={lists} />
           <div className="sad dotted"></div>
-          <Price price="$20/day" />
+          <Price price="$20" date="/day" />
           <Button />
         </div>
         <div className="card-3">
-          <CardTitle text="Monthly Pass" />
+          <CardTitle text=" Monthly Pass" icon={true} />
           <CardH2 text="What You'll Get" />
           <CardList lists={lists2} />
           <div className="sad dotted"></div>
-          <Price price="$380/month" />
+          <Price price="$380" date="/month" />
           <Button />
         </div>
       </div>
--- lessons/37/second-try/my-app/src/components/card-title/card-title.tsx
+++ lessons/37/second-try/my-app/src/components/card-title/card-title.tsx
@@ -2,13 +2,17 @@ import React from "react";
 
 interface CardTitleProps {
   text: String;
+  icon: boolean;
 }
 
 export const CardTitle: React.FC<CardTitleProps> = (props) => {
-  return (
-    <h1>
-      <i className="material-icons ">face </i>
-      {props.text}
-    </h1>
-  );
+  if (props.icon === true) {
+    return (
+      <h1>
+        <i className="material-icons ">face </i>
+        {props.text}
+      </h1>
+    );
+  }
+  return <h1>{props.text}</h1>;
 };
