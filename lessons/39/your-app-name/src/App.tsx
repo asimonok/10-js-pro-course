c567e125de2bc0aeb493a8af363419d130e3e155
FolseV <folsefeeder@gmail.com>
2021-10-12 17:13:06 +0300

    new branch, fetch(reworked app file)

---
+++ lessons/39/your-app-name/src/App.tsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import "./App.css";
+import EmojiContainer from "./components/EmojiContainer";
+import SearchInput from "./components/SearchInput";
+import SearchNumber from "./components/SearchNumber";
+
+// interface Props {}
+// interface State {
+//   searchValue: string;
+//   resultNumber: number;
+// }
+
+const App = () => {
+  // const [state, setState] = useState({
+  //   searchValue: "",
+  //   resultNumber: 100,
+  // });
+  const [searchValue, setSearchValue] = useState("");
+  const [resultNumber, setResultNumber] = useState(100);
+
+  // state: State;
+
+  // constructor(props: Props) {
+  //   super(props);
+
+  //   this.state = {
+  //     searchValue: "",
+  //     resultNumber: 100,
+  //   };
+  // }
+
+  // valueChangeHandler = (newValue: string): void => {
+  //   this.setState({ searchValue: newValue });
+  // };
+
+  // const resultNumberHandler = (newNumber: number):void => {
+  //   setState(() => resultNumber: newNumber)
+  // }
+  const resultNumberHandler = (newNumber: number): void => {
+    setResultNumber(newNumber);
+  };
+
+  const valueChangeHandler = (newValue: string): void => {
+    setSearchValue(newValue);
+  };
+
+  // render() {
+  return (
+    <>
+      <div className="search">
+        <SearchNumber
+          // searchNumber={this.state.resultNumber}
+          searchNumber={resultNumber}
+          // numberRes={(newNumber) => this.setState({ resultNumber: newNumber })}
+          numberRes={resultNumberHandler}
+        />
+        <SearchInput
+          // searchValue={this.state.searchValue}
+          searchValue={searchValue}
+          searchValueChange={valueChangeHandler}
+          // searchValueChange={(newValue) =>
+          //   this.setState({ searchValue: newValue })
+          // }
+        />
+      </div>
+
+      <EmojiContainer
+        // searchValue={this.state.searchValue}
+        searchValue={searchValue}
+        // resultNumber={this.state.resultNumber}
+        resultNumber={resultNumber}
+      />
+    </>
+  );
+};
+// }
+
+export default App;
