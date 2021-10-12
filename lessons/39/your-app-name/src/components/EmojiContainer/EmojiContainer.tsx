7da55947327ded2c0f17961f1715fc818d04945c
FolseV <folsefeeder@gmail.com>
2021-10-12 17:38:54 +0300

    feat[EmojiContainer]: reworked to hooks

---
+++ lessons/39/your-app-name/src/components/EmojiContainer/EmojiContainer.tsx
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from "react";
+import EmojiRow from "../EmojiRow";
+import "./EmojiContainer.css";
+
+interface Props {
+  searchValue: string;
+  resultNumber: number;
+}
+// interface State {
+//   emojiList: Emoji[];
+// }
+interface Emoji {
+  title: string;
+  symbol: string;
+  keywords: string;
+}
+
+// class EmojiContainer extends React.Component {
+const EmojiContainer: React.FC<Props> = (props: Props) => {
+  // props: Props;
+  // state: State;
+
+  // // constructor(props: Props) {
+  //   super(props);
+
+  //   this.props = props;
+  //   this.state = {
+  //     emojiList: [],
+  //   };
+  // }
+  const [state, setState] = useState({ emojiList: [] });
+  function filterEmojis(searchValue: string, emojiList: Emoji[]): Emoji[] {
+    const filteredemoji = emojiList.filter(
+      (emoji) =>
+        emoji.title.includes(searchValue) ||
+        emoji.keywords.includes(searchValue) ||
+        emoji.symbol.includes(searchValue)
+    );
+    return filteredemoji.slice(0, props.resultNumber);
+  }
+
+  useEffect(() => {
+    fetch(
+      "https://raw.githubusercontent.com/FolseV/js-pro-course/lessons/38/lessons/38/emojiList.json"
+    )
+      // .then((res): Promise<Emoji[]> => {
+      .then((res) => {
+        return res.json();
+      })
+      // .then((emojiList: Emoji[]): Emoji[] => {
+      .then((emojiList) => {
+        setState({ emojiList });
+      })
+      .catch((error) => console.log(error));
+  });
+  /*
+  CopmonentDidMount() {
+    fetch(
+      "https://raw.githubusercontent.com/FolseV/js-pro-course/lessons/38/lessons/38/emojiList.json"
+    )
+      .then((res): Promise<Emoji[]> => {
+        return res.json();
+      })
+      // .then((emojiList: Emoji[]): Emoji[] => {
+      .then((emojiList) => {
+        this.setState({ emojiList });
+      })
+      .catch((error) => console.log(error));
+  }
+*/
+  // let filteredList = this.filterEmojis(this.props.searchValue, this.state.emojiList)
+
+  // render() {
+  // const filteredList = this.filterEmojis(
+  //   this.props.searchValue,
+  //   this.state.emojiList
+  // );
+  const filteredList = filterEmojis(props.searchValue, state.emojiList);
+  return (
+    <>
+      <ul className="row">
+        {filteredList.map((el) => {
+          return (
+            <li key={el.title}>
+              <EmojiRow emoji={el} />
+            </li>
+          );
+        })}
+      </ul>
+    </>
+  );
+  // }
+};
+
+export default EmojiContainer;
