f84a28ebed2c7bb07336eda1f1d2df921db1e5f3
volha bukhal <o.bukhal@gmail.com>
2021-10-13 19:51:18 +0300

    feat: add Input component in App

--- lessons/38/emoji-fetch-hw38/src/App.tsx
+++ lessons/38/emoji-fetch-hw38/src/App.tsx
@@ -1,20 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 import EmojiContainer from './components/EmojiContainer'
-import Clock from './components/Clock'
 import Input from './components/Input'
 
-// здесь должно быть значение текущего поиска
+interface Props{}
 
-function App() {
-
-  return (
-    <div className="App">
-        <Clock/>
-        <EmojiContainer/>
-    </div>
-  );
+interface State {
+  searchingWord: string
 }
 
+class App extends Component<Props, State> {
+  constructor(props: Props){
+    super(props);
+    this.state={
+      searchingWord: '',
+    }   
+  }
+
+  updateSearchingWord = (newSearchingWord: string) => {
+    this.setState({
+      searchingWord: newSearchingWord,
+    })
+  }
+
+  render() {
+
+    return (
+      <div className="App">
+        <Input 
+          searchingWord={this.state.searchingWord} 
+          onChange={this.updateSearchingWord} 
+        />
+        <EmojiContainer searchingWord={this.state.searchingWord} />
+      </div>
+    )
+
+  }
+}
 
 export default App;
--- lessons/38/emoji-fetch-hw38/src/components/EmojiContainer/EmojiContainer.tsx
+++ lessons/38/emoji-fetch-hw38/src/components/EmojiContainer/EmojiContainer.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import './EmojiContainer.css';
 import EmojiRow from '../EmojiRow'
-import Input from '../Input'
 
 interface Emoji {
     title: string;
@@ -12,11 +11,11 @@ interface Emoji {
 interface State {
     emojiList: Array<Emoji>;
     emojiFiltered: Array<Emoji>;
-    searchingWord: string,
 }
 
-interface Props {}
-
+interface Props {
+    searchingWord: string
+}
 
 class EmojiContainer extends React.Component<Props, State> {
     constructor(props:any) {
@@ -24,11 +23,9 @@ class EmojiContainer extends React.Component<Props, State> {
         this.state = {
             emojiList: [],
             emojiFiltered:[],
-            searchingWord: '',
         }
     }
 
-    //in time of mounting DOM make fetch request
     componentDidMount() {
         const emojiList = fetch('https://raw.githubusercontent.com/asimonok/10-js-pro-course/lesson/38/lessons/38/emojiList.json');
         emojiList.then( data => data.json())
@@ -39,41 +36,24 @@ class EmojiContainer extends React.Component<Props, State> {
         })
     }
 
-    // filterEmoji = (event: React.ChangeEvent<HTMLInputElement>) => {
-
-    //     let arr = this.state.emojiList;
-        
-    //     let filteredArr = arr.filter( (item) => item.title.includes(this.state.searchingWord) || item.keywords.includes(this.state.searchingWord)  );
-
-    //     this.setState( {
-    //         emojiFiltered: filteredArr
-    //     })
-    // }
-
-    updateSearchingWord = (newSearchingWord: string) => {
-
-        let word = newSearchingWord
+    filterEmoji = () => {
+        const {searchingWord} = this.props 
         let arr = this.state.emojiList;
-        let filteredArr = arr.filter( (item) => item.title.includes(word) || item.keywords.includes(word)  );
+        let filteredArr = arr.filter( (item) => item.title.includes(searchingWord) || item.keywords.includes(searchingWord) );
 
         this.setState( {
             emojiFiltered: filteredArr,
-            searchingWord: newSearchingWord
         })
     }
-
+        
     render() {
         const{emojiFiltered} = this.state;
+
         return (
             <>
             <br></br>
-
-            <Input searchingWord={this.state.searchingWord} onChange={this.updateSearchingWord} />
-
-            <br></br>
             <div><i>there are</i> {emojiFiltered.length} emoji</div>
             <br></br>
-
             <ul>{ emojiFiltered.slice(0,10).map( (item, i) => <EmojiRow key={i} item={item}/>) }</ul>
             </>
         )
