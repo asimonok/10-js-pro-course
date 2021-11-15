768ece26f0be2bc7d58ebb32d1128f5932d2e99e
Irina <irenserge@mail.ru>
2021-11-15 19:24:24 +0300

    feat (hw46) add reducer for posts

--- lessons/40/my-app/src/store/UserReducer.ts
+++ lessons/40/my-app/src/store/UserReducer.ts
@@ -1,22 +1,21 @@
 import {AuthorTypes} from '../types/AuthorTypes';
 import {PostTypes} from '../types/PostTypes';
-import {UserPostsAction} from '../store/UserPostsReduser';
 import {Action} from 'redux';
 import { Dispatch} from "react";
 
 
 export enum UserActionType {
-    FETCH_USERS = 'FETCH_USERS',
-    FETCH_USERS_SUCCESS = 'FETCH_USERS_SUCCESS',
+    FETCHING_USERS = 'FETCHING_USERS',
+    FETCHED_USERS = 'FETCHED_USERS',
 }
 
 export const fetchUsers = () => {
     return (dispatch: Dispatch<UserAction>) => {
-        dispatch({type: UserActionType.FETCH_USERS})
+        dispatch({type: UserActionType.FETCHING_USERS})
         fetch(`https://jsonplaceholder.typicode.com/users`)
           .then((response):Promise<AuthorTypes[]> => response.json())
           .then( users => {
-            dispatch({type: UserActionType.FETCH_USERS_SUCCESS, payload: users})
+            dispatch({type: UserActionType.FETCHED_USERS, payload: users})
         }).catch(error => {
             console.log(error);
         })
@@ -28,10 +27,10 @@ export type ActionPayload <TypeAction, TypePayload> = {
     payload: TypePayload;
 }
 
-export type FetchUserAction = Action<UserActionType.FETCH_USERS>
-export type FetchSuccessUserAction = ActionPayload<UserActionType.FETCH_USERS_SUCCESS, AuthorTypes[]>
+export type FetchUserAction = Action<UserActionType.FETCHING_USERS>
+export type FetchSuccessUserAction = ActionPayload<UserActionType.FETCHED_USERS, AuthorTypes[]>
 
-export type UserAction = FetchUserAction | FetchSuccessUserAction;
+export type UserAction = FetchUserAction | FetchSuccessUserAction | FetchUserAction;
 
 type State = {
     users: AuthorTypes[],
@@ -45,55 +44,16 @@ const initialState: State = {
     error: ''
 }
 
-export enum UserPostsActionType {
-    FETCHING_USER_POSTS = 'FETCHING_USER_POSTS',
-    FETCHED_USER_POSTS = 'FETCHED_USER_POSTS',
-    FETCHING_USER = 'FETCHING_USER',
-    FETCHED_USER = 'FETCHED_USER',
-    FETCHING_ERROR = 'FETCHING_ERROR',
-}
-
-export const fetchUserPosts= (userId:string) => {
-    return (dispatch: Dispatch<UserPostsAction>) => {
-        dispatch({type: UserPostsActionType.FETCHING_USER_POSTS})
-        fetch(`https://jsonplaceholder.typicode.com/posts?userId=${userId}`)
-        .then((response):Promise<PostTypes[]> => {
-            if(response.ok) {
-                return response.json()
-            }
-            dispatch({type: UserPostsActionType.FETCHING_ERROR, payload: true})
-            throw new Error('error ...')})
-        .then(posts => {
-            dispatch({type: UserPostsActionType.FETCHED_USER_POSTS, payload: posts})
-        })
-        .catch(error => { 
-            console.log(error)
-        });
-    }
-}
-
-export const fetchUser= (userId:string) => {
-    return (dispatch: Dispatch<UserPostsAction>) => {
-        dispatch({type: UserPostsActionType.FETCHING_USER})
-        fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
-        .then((response):Promise<AuthorTypes> => response.json())
-        .then(user => {
-            dispatch({ type:UserPostsActionType.FETCHED_USER, payload: user })
-        })
-        .catch(error => console.log(error));
-    }
-}
-
 export const UserReducer = (state: State = initialState, action: UserAction): State => {
     switch(action.type) {
-        case UserActionType.FETCH_USERS: {
+        case UserActionType.FETCHING_USERS: {
             return {
                 ...state,
                 users: [],
                 loading: true,
             }
         }
-        case UserActionType.FETCH_USERS_SUCCESS: {
+        case UserActionType.FETCHED_USERS: {
             return {
                 ...state,
                 users: [...state.users, ...action.payload],
--- lessons/40/my-app/src/store/store.ts
+++ lessons/40/my-app/src/store/store.ts
@@ -1,14 +1,12 @@
 import { createStore, combineReducers, applyMiddleware } from "redux";
 import {UserReducer} from './UserReducer';
 import { PostReducer} from './PostReducer';
-import {UserPostsReducer} from './UserPostsReduser';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import thunk from 'redux-thunk';
 
 const RootReducer = combineReducers({
     users: UserReducer, 
     posts: PostReducer,
-    userPosts: UserPostsReducer
 })
 
 export type RootState = ReturnType <typeof RootReducer>;
